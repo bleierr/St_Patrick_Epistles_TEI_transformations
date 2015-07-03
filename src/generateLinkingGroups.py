9527a12e0583e86102c5f7091440abdfbf2e6978
unknown <bleierr@shh122195.college.tcd.ie>
2015-07-03 11:16:18 +0100

    Added a create lines function The script was changed to split an xml file along empty elements lb. Bugs: still many bugs and not tested with a real file

--- src/generateLinkingGroups.py
+++ src/generateLinkingGroups.py
@@ -7,6 +7,8 @@ from lxml import etree
 from StringIO import StringIO
 import re
 
+xml_ns = "http://www.w3.org/XML/1998/namespace"
+
 def open_xml_file(xml_file):
     #do not encode in unicode when opening, e.g. with codecs module
     with open(xml_file, "r") as f:
@@ -19,8 +21,6 @@ def open_xml_file(xml_file):
             print "Error in open_xml_file function:"
             print e
             
-def get_elements_to_compare(xml_tree_element, xpath_condition):
-    return xml_tree_element.xpath(xpath_condition)
 
 def compare_two_elements(e1, e2):
     return e1.text == e2.text
@@ -34,18 +34,81 @@ def apply_xslt(xml, xslt):
     xslt_root = etree.XML(xslt)
     transform = etree.XSLT(xslt_root)
     return transform(xml)
+
+def apply_filter(xml, pat):
+    return xml_tree_element.xpath(xpath_condition)
                 
 
+def generate_valid_xml(strg, tag_lst):
+    for tag in tag_lst:
+
+        strg = "".format()
+        try:
+            tree = etree.fromstring(strg)
+            return tree
+        except etree.XMLSyntaxError as e:
+            print "Error in generate_valid_xml function:"
+            print e
+            
+            continue
     
+
+def generate_valid_xml_line(line_strg, lb1, lb2=None):
+    p1 = lb1.getparent() 
+
+    if p1.tag not in ["text", "w"]:
+        print("Error: lb {} is not correctly nested!".format(lb1.attrib["{%s}id" % xml_ns]))
+        print(p1.tag)
+    else:
+        if p1.tag == "w":
+            pline = p1.getparent()
+            line_strg = """<w xml:id="{0}" lemma="{1}"
+                        type="{2}">{3}""".format(p1.attrib["{%s}id" % xml_ns],
+                                                 p1.attrib["lemma"],
+                                                 p1.attrib["type"],
+                                                 line_strg)
+        
+    if lb2 is not None:
+        p2 = lb2.getparent()
+        if p2.tag not in ["text", "w"]:
+            print("Error: lb {} is not correctly nested!".format(lb1.attrib["{%s}id" % xml_ns]))
+            print(p2.tag)
+        if p2.tag == "w":
+            line_strg = """{0}</w>""".format(line_strg)
     
-
-
-
-
-
+            
+    return """<line>{0}</line>""".format(line_strg)
 
 
 
 
 if __name__ == '__main__':
-    pass
+    xml_files = []
+
+    wit = {}
+    for x in xml_files:
+        tree = open_xml_file(x)
+        lbs = tree.xpath("\\lb[@ed='White']")
+
+        wit[x] = {}
+        for idx, lb in enumerate(lbs):
+
+            with open(x, "r") as f:
+                xml_strg = f.read()
+                rest_strg = slice_strg(xml_strg, etree.tostring(lb))[1]
+                line_strg = slice_strg(rest_strg, etree.tostring(lbs[idx+1]))[0]
+
+            following_lb = lbs[idx+1]
+
+            line_strg = generate_valid_xml_line(lb, following_lb)
+ 
+            wit[x][lb.attrib["xml:id"]] = line_strg
+
+            
+
+        
+
+        
+
+
+    
--- src/test_generateLinkingGroups.py
+++ src/test_generateLinkingGroups.py
@@ -8,17 +8,39 @@ import os
 import generateLinkingGroups as glg
 from lxml import etree
 
+xml_ns = "http://www.w3.org/XML/1998/namespace"
+
 xml_1 = """<?xml version="1.0" encoding="UTF-8" ?>
+            
             <tei>
-                <header><title>this is the title</title></header>
-                <text><lb id="1"/><p id="1-1">par 1</p><lb id="2"/><p id="1-2">par 2</p><lb id="3"/><p id="1-3">par 3</p></text>
+                <header><title>example text 1</title></header>
+                <text>
+                    <lb xml:id="lb1" ed="white"/>
+                    <w xml:id="w1-1" lemma="per" type="prep">per</w>
+                    <lb xml:id="lb2" ed="white"/>
+                    <w xml:id="w1-2" lemma="nomen" type="noun">nomen</w>
+                    <lb xml:id="lb3" ed="white"/>
+                    <w xml:id="w1-3" lemma="per" type="prep"><g>P</g>er</w>
+                    <w xml:id="w1-4" lemma="rusticus" type="adj"><g>r</g>usticus</w>
+                    <w xml:id="w1-5" lemma="unknown" type="unknown">patricii</w>
+                </text>
             </tei>
             """
             
 xml_2 = """<?xml version="1.0" encoding="UTF-8" ?>
+            
             <tei>
-                <header><title>this is the title</title></header>
-                <text><lb id="1"/><p id="2-1">par 1</p><lb id="2"/><p id="2-2">par 2</p><lb id="3"/><p id="2-3">par3</p></text>
+                <header><title>example text 1</title></header>
+                <text>
+                    <lb xml:id="lb1" ed="white"/>
+                    <w xml:id="w2-1" lemma="per" type="prep">per</w>
+                    <w xml:id="w2-2" lemma="Patricius" type="name">Patricius</w>
+                    <lb xml:id="lb2" ed="white"/>
+                    <w xml:id="w2-3" lemma="nomen" type="noun">nominis</w>
+                    <lb xml:id="lb3" ed="white"/>
+                    <w xml:id="w2-4" lemma="unknown" type="unknown">per</w>
+                    <w xml:id="w2-5" lemma="rusticus" type="adj">rusticussimus</w>
+                </text>
             </tei>
             """
             
@@ -32,11 +54,20 @@ xslt_1 = """<?xml version="1.0" encoding="UTF-8"?>
             
             <xsl:template match="header">
             </xsl:template>
+
+            <xsl:template match="g">
+                <xsl:apply-templates />
+            </xsl:template>
+
+            <xsl:template match="ex">
+                <xsl:apply-templates />
+            </xsl:template>
             
             </xsl:stylesheet>
         """
 
-level_of_comparison = "//p"            
+level_of_comparison = "//p"
+
             
 cur_dir = os.getcwd()
 
@@ -69,29 +100,20 @@ class Test(unittest.TestCase):
         t2 = glg.open_xml_file(xml_file_2)
         self.assertTrue(isinstance(t2, etree._ElementTree), "The variable t should be a lxml _ElementTree element but is: {}".format(t2))
 
-    def test_get_elements_to_compare(self):
-        t1 = glg.open_xml_file(xml_file_1)
-        elements1 = glg.get_elements_to_compare(t1, level_of_comparison)
-        self.assertTrue(isinstance(elements1, list), "The variable elements1 should be a list, but is: {}".format(elements1))
-        for e in elements1:
-            self.assertTrue(isinstance(e, etree._Element), "The variable elements1 should be a lxml _Element, but is: {}".format(e))
-
     def test_compare_two_elements(self):
-        t1 = glg.open_xml_file(xml_file_1)
-        elements1 = glg.get_elements_to_compare(t1, level_of_comparison)
-        t2 = glg.open_xml_file(xml_file_2)
-        elements2 = glg.get_elements_to_compare(t2, level_of_comparison)
-        for idx, t in enumerate(zip(elements1, elements2)):
-            x = t[0]
-            y = t[1]
-            r = glg.compare_two_elements(x, y)
-            if idx < 2:
-                self.assertTrue(r, "Error: function compare_two_elements should return True, but returned: {0}!".format(r))
-            else:
-                self.assertFalse(r, "Error: function compare_two_elements should return False, but returned: {0}!".format(r))
+        e1 = etree.Element("p")
+        e1.text = "some text"
+        e2 = etree.Element("p")
+        e2.text = "some ext"
+        r = glg.compare_two_elements(e1, e2)
+        self.assertFalse(r, "Error: function compare_two_elements with the param ({0}, {1}) should return False, but returned: {2}".format(e1.text, e2.text, r))
+        e2.text = "some text"
+        r = glg.compare_two_elements(e1, e2)
+        self.assertTrue(r, "Error: function compare_two_elements with the param ({0}, {1}) should return True, but returned: {2}".format(e1.text, e2.text, r))
+        
     
     def test_slice_strg(self):
-        pat = """<lb id="\d"/>"""
+        pat = """<lb xml:id="lb\d" ed="\w+"/>"""
         with open(xml_file_1, "r") as f:
             strgs = glg.slice_strg(f.read(), pat)
         self.assertTrue(isinstance(strgs, list), "The variable strgs should be a list, but is: {}".format(strgs))
@@ -100,12 +122,45 @@ class Test(unittest.TestCase):
             
         
         #the first element contains all the xml data before the first <lb/> element
-        self.assertEqual(strgs[1], """<p id="1-1">par 1</p>""", """Error: the string {0} does not match <p id="1-1">par 1</p>!""".format(strgs[0]))
+        self.assertEqual(strgs[1].strip(), """<w xml:id="w1-1" lemma="per" type="prep">per</w>""", """Error: the string {0} does not match
+                                    <w xml:id="w1-1" lemma="per" type="prep">per</w>""".format(strgs[1].strip()))
         
     def test_apply_xslt(self):
         t1 = glg.open_xml_file(xml_file_1)
         x = glg.apply_xslt(t1, xslt_1)
-        print(type(x))
+        self.assertTrue(isinstance(x, etree._XSLTResultTree), "The variable x should be a lxml _XSLTResultTree, but is: {}".format(x))
+        w13 = x.xpath("//w")[2]
+        self.assertEqual("w", w13.tag, "Error: the element tag of p13 should be 'w', but is: {0}".format(w13.tag))
+        self.assertEqual("w1-3", w13.attrib["{%s}id" % xml_ns], "Error: the value of attribute @xml:id should be 'w1-3', but is: {0}".format(w13.attrib["{%s}id" % xml_ns]))
+        self.assertEqual("Per", w13.text, "Error: the content of the element should be the sting 'Per', but is: '{0}'".format(w13.text))
+        #print(t1.xpath("//w[3]/text()"))
+        #print(etree.tostring(p13))
+        self.assertEqual("""<w xml:id="w1-3" lemma="per" type="prep">Per</w>""", etree.tostring(w13).strip(), """Error: the converstion of p13
+                            to string should result in the string '<w xml:id="w1-3" lemma="per" type="prep">Per</w>', but is: '{0}'""".format(etree.tostring(w13).strip()))
+
+    def test_generate_valid_xml_line(self):
+        t1 = glg.open_xml_file(xml_file_1)
+
+        lbs = t1.xpath("//lb")
+        xml_strg = etree.tostring(t1)
+        lst = []
+
+        for idx, lb in enumerate(lbs):
+            lb1 = lb
+            print (idx)
+            print (idx < len(lbs))
+            if idx < len(lbs)-1:
+                lb2 = lbs[idx+1]
+                rest_strg = glg.slice_strg(xml_strg, etree.tostring(lb1))[1]
+                line_strg = glg.slice_strg(rest_strg, etree.tostring(lb2))[0]
+                lst.append(glg.generate_valid_xml_line(line_strg, lb1, lb2))
+            elif idx == len(lbs)-1:
+                rest_strg = glg.slice_strg(xml_strg, etree.tostring(lb1))[1]
+                lst.append(glg.generate_valid_xml_line(rest_strg, lb1))
+        
+        print(lst)
+        
+        
     
 
 if __name__ == "__main__":
